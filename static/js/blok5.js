265ff52bf8e487dd9df9ccd2c0c2b709c1fa32cd
unknown <222111886@stis.ac.id>
2024-01-06 11:33:00 +0700

    Update Font

--- static/js/blok5.js
+++ static/js/blok5.js
@@ -83,7 +83,7 @@ const config5 = {
                 render: 'percentage',
                 precision: 2,
                 fontColor: 'black',
-                fontSize: 10,
+                fontSize: 9.5,
                 align:'center',
                 position: 'center',
                 offset: 0,
